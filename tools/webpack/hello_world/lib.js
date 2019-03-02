0589b76b6387a38380317ac237c27314047b27c0
FedorSymkin <fedorsymkin52@gmail.com>
2019-03-02 22:39:45 +0300

    webpack

--- tools/webpack/hello_world/lib.js
+++ tools/webpack/hello_world/lib.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//Важно: здесь надо обязательно указать export, чтбоы webpack всё правильно собрал
+
 export function make_bold(x) {
     return '<b>' + x + '</b>';
 }
+
--- tools/webpack/hello_world/webpack.config.js
+++ tools/webpack/hello_world/webpack.config.js
@@ -1,8 +1,13 @@
 'use strict';
 
 module.exports = {
+  //js точка входа
   entry: './entry.js',
+
+  //у меня по умолчанию генерируется в dist/build.js
   output: {
     filename: 'build.js'
   }
+
 };
+
