39ceab65adfa4af1558c9d03fadfaf46130f95bd
FedorSymkin <fedorsymkin52@gmail.com>
2019-07-21 22:09:52 +0300

    C++ network

--- languages/C++/network/src/main.cpp
+++ languages/C++/network/src/main.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
-#include <gtest/gtest.h>
+#include "common.h"
+
+//http://masandilov.ru/network/guide_to_network_programming
 
 int main(int argc, char **argv) {
-    if (argc == 2 && std::string(argv[1]) == "--ut") {
-        /*
-        sudo apt-get install libgtest-dev
+    /*
+    sudo apt-get install libgtest-dev
 
-        cd /usr/src/gtest
-        sudo cmake CMakeLists.txt
-        sudo make
-        sudo cp *.a /usr/lib
-        */
-        testing::InitGoogleTest(&argc, argv);
-
-        return RUN_ALL_TESTS();
-    }
-
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+    cd /usr/src/gtest
+    sudo cmake CMakeLists.txt
+    sudo make
+    sudo cp *.a /usr/lib
+    */
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
